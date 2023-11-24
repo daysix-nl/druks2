53b10d17f33e4cbc945057d6dc50b260c1d938c6
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-11-24 18:34:36 +0100

    wip

---
+++ page-templates/bestellen-duo-free.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Template name: Bestellen DUO FREE
+ */
+
+
+ get_header(); ?>
+
+<main>
+    
+
+
+</main>
+
+
+
+<main class="">
+    <section class="bg-[#012F8C] relative py-[13vw] md:py-[8vw] overflow-hidden">
+        <div class="w-[95vw] md:w-[70vw] md:ml-[8vw] mx-auto md:mx-[unset] z-[50] relative">
+            <h1 class="text-[14vw] leading-[13vw] md:text-[5.7vw] md:leading-[6.7vw] text-white uppercase font-bison">Zo krijg je geld <br class="md:hidden"><span class="text-[#FF008A]">terug</span> van DUO</h1>
+            <p class="text-[4vw] leading-[5.9vw] md:text-[1.2vw] md:leading-[1.9vw] text-white font-satoshi font-bold mt-[5vw] md:mt-[1vw] mr-[2.5vw]">In dit hoofdstuk leg ik je uit hoe het mij gelukt is om in aanmerking te komen voor de DUO-regeling waarbij een deel van mijn studieschuld is kwijtgescholden vanwege mijn ADHD(/ADD/AUTISME/DEPRESSIE). Daarnaast krijg je ook de bijbehorende formulieren. Dat scheelt jou weer tijd en energie. Die je mooi kunt stoppen in het opstarten van de procedure die volledig staat uitgelegd in dit hoofdstuk. Zeker weten of je in aanmerking komt voor het hoofdstuk, bekijk dan even de veelgestelde vragen.<br><br>Vul je gegevens in zodat het hoofd stuk, en de bijbehorende formulieren, direct in je inbox terechtkomen. </p>
+        </div>
+        <div class="w-[95vw] md:w-[49vw] mx-auto md:mx-[unset] md:ml-[8vw] z-[50] relative form pt-[7vw] md:pt-[1.5vw]">
+            <?php echo do_shortcode('[gravityform id="7" title="false" description="false"]'); ?>
+        </div>
+        <img src="/wp-content/themes/druks2/img/local/background.png" alt="" class="absolute min-h-full min-w-full object-cover object-center top-0 left-0 right-0">
+    </section>
+
+      
+  <section class="w-[100vw] h-[180vw] md:h-[55.64vw] overflow-hidden relative">
+        <img class="h-full object-cover object-center absolute top-0 left-0 w-full" src="/wp-content/themes/druks2/img/local/bg-druks.png" alt="">
+    </section>
+
+    <section class="bg-green py-[15vw] md:py-[7vw]">
+        <h2 class="text-[12vw] leading-[12vw] md:text-[5.7vw] md:leading-[6vw] text-[#012F8C] uppercase font-bison relative z-[56] text-center mx-auto bg-white w-fit px-[3vw]"><span class="pb-[0.2vw]">Contact</span></h2>
+        <p class="text-[4.5vw] leading-[5.9vw] md:text-[1.61vw] md:leading-[2.2vw] text-[#000000] font-satoshi font-bold max-w-[79vw] md:max-w-[55vw] mt-[8vw] md:mt-[3vw] text-center mx-auto">Vragen? Interesse? Neem contact met mij op via onderstaand mailadres om de mogelijkheden te bespreken.</p>
+        <a href="mailto:hello@francienregelink.nl" class="text-[7vw] md:text-[2.7vw] uppercase font-bison bg-[#012F8C] text-[#fff] md:hover:bg-[#FF93C1] md:hover:text-[#CDF200] rounded-[11px] px-[8vw] md:px-[4vw] py-[0vw] flex items-center w-fit mt-[8vw] md:mt-[3vw] mx-auto md:mx-auto"><span class="mb-[0.2vw]">hello@francienregelink.nl</span></a>
+    </section>
+    
+</main>
+
+<?php get_footer(''); ?>
