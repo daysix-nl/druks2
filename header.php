8b67efeb27aa298271d2889c6ad421892b7f94c2
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2023-12-01 13:49:36 +0100

    wip

--- header.php
+++ header.php
@@ -10,7 +10,8 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <link href="https://db.onlinewebfonts.com/c/d7e06b3006c000446ba07df1d4955920?family=Bison+Bold" rel="stylesheet">
+    <link rel="preload" as="font" href="/wp-content/themes/druks2/fonts/BisonBold.woff2" type="font/woff2" crossorigin="anonymous">
+    <!-- <link href="https://db.onlinewebfonts.com/c/d7e06b3006c000446ba07df1d4955920?family=Bison+Bold" rel="stylesheet"> -->
     <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
     <title><?php bloginfo( 'name' ); ?></title>
     <?php wp_head(); ?>
