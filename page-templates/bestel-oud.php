03a9f8dc1c044ca9ae5c8b5f5b66b4974bb52abb
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2024-01-05 15:32:55 +0100

    wip

--- page-templates/bestel-oud.php
+++ page-templates/bestel-oud.php
@@ -16,6 +16,7 @@
 Francien spijkert je bij over hoe je nog meer uit je AD(H)D kunt halen, door dingen op geheel eigen’wijze’ uit te vogelen. Ook het succesvolle TIKKIE TERUG AAN DUO hoofdstuk vind je terug in dit boek. 
 <br><br>
 DRUKS 2 is zowel een trap onder je kont als een arm om je heen. Eén ding is zeker: na het lezen van dit boek zal je AD(H)D nooit meer hetzelfde zijn.</p>
+<br><br>Bestel via onderstaand formulier. </p>
                 
             </div>
             
