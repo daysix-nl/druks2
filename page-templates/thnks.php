42ebbb5c7b9f3a18081facbcf700a189159dcc16
Kevin Regelink <109856528+daysix-nl@users.noreply.github.com>
2024-08-28 15:21:51 +0200

    wip

--- page-templates/thnks.php
+++ page-templates/thnks.php
@@ -13,7 +13,7 @@
                 <h1 class="text-[11vw] leading-[11vw] md:text-[5.7vw] md:leading-[6.7vw] text-white uppercase font-bison text-center">De Drukskoerier komt zo je kant op!</h1>
             </div>
             <div class="w-[80vw] md:w-[50vw] mx-auto z-[99] relative">
-                <img src="/wp-content/themes/druks2/img/local/ezgif-1-738b510f54.gif" alt="" class="w-full">
+                <img src="/wp-content/themes/druks2/img/local/drukskoerier.gif" alt="" class="w-full">
             </div>
         </div>
          <img src="/wp-content/themes/druks2/img/local/background.png" alt="" class="absolute min-h-full min-w-full object-cover object-center top-0 left-0 right-0">
